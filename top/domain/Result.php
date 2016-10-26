42793513a73cd097b992bf0e1fc77b9f6dcb4288
songyichao <songyichao@mioji.com>
2016-10-26 23:13:31 +0800

    complete

---
+++ top/domain/Result.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace yii\taobao\top\domain;
+/**
+ * 无
+ * @author auto create
+ */
+class Result
+{
+	
+	/** 
+	 * 错误码
+	 **/
+	public $code;
+	
+	/** 
+	 * 无
+	 **/
+	public $model;
+	
+	/** 
+	 * 原因
+	 **/
+	public $msg;
+	
+	/** 
+	 * 结果
+	 **/
+	public $success;	
+}
+?>
